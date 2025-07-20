e0b46315c30aa167bdc6aa853f5747fb3815e727
Zeeshan <mdzishanc@gmail.com>
2025-07-21 02:29:54 +0530

    Added Header

--- frontend/vite-project/src/components/Header.jsx
+++ frontend/vite-project/src/components/Header.jsx
@@ -1,31 +1,60 @@
 import "./Header.css";
+import { useState } from "react";
 
 function Header() {
+  const [isFocused, setIsFocused] = useState(false);
 
-   
   return (
     <div className="headerContainer">
       <ul className="mainUl">
+        {/* Left Section */}
         <li className="firstPart">
-          <span></span>
-          <h1><i className="fa fa-youtube-play" style={{ color: "red", fontSize: "20px" }}></i>Youtube</h1>
+          <button className="navBar">
+            <i className="fa fa-bars"></i>
+          </button>
+          <h1 className="youtubeBrand">
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+    alt="YouTube Logo"
+    className="youtubeLogo"
+  />
+</h1>
         </li>
-        <li className="secondPart">
-          <form>
-            <input type="text" placeholder="Search" name="Search" ></input>
-            <button type="submit:" className="searchButton" >
-              <i className="fa fa-search " style={{fontSize:"27.5px", color:"white", padding:"0px" }}></i>
-            </button>
-            <button><i className="fa fa-microphone" style={{ fontSize: "20px", color: "white" }}></i>
 
-            
+        {/* Middle/Search Section */}
+        <li className="secondPart">
+          <form className={`searchForm ${isFocused ? "focused" : ""}`}>
+            {isFocused && (
+              <span className="leftSearchIcon">
+                <i className="fa fa-search"></i>
+              </span>
+            )}
+            <input
+              type="text"
+              placeholder="Search"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+            <button type="submit" className="searchButton">
+              <i className="fa fa-search"></i>
+            </button>
+            <button className="micButton">
+              <i className="fa fa-microphone"></i>
             </button>
           </form>
         </li>
+
+        {/* Right/User Section */}
         <li className="thirdPart">
-          <button>Create</button>
-          <button>Bell-icon</button>
-          <span>Profile</span>
+          <button className="create">
+            <span className="plus">+</span> Create
+          </button>
+          <button className="bell">
+            <i className="fa fa-bell"></i>
+          </button>
+          <button className="profile">
+            <i className="fa fa-user"></i>
+          </button>
         </li>
       </ul>
     </div>
